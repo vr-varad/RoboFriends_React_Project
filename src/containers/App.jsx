6a57f5e4739eb55f362ce5eccc2baf2841099ba1
Varad Gupta <varadgupta21@gmail.com>
2023-11-22 01:16:38 +0530

    Added useState and useEffect Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,48 +1,41 @@
 // eslint-disable-next-line no-unused-vars
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import './App.css'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary'
 
-class App extends Component {
+function App(){
 
-  constructor(){
-    super()
-    this.state = {
-      robots: [],
-      searchFeild: ''
-
-    }
-  }
-
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
+  const [robots, setRobots] = useState([])
+  const [searchFeild,setSearchFeild] = useState('')
+  
+  useEffect(()=>{
+     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(user => this.setState({robots: user}))
+    .then(user => setRobots(user))
+  },[])
+
+   const onSearchChange = (event) => {
+    setSearchFeild(event.target.value)
   }
 
-   onSearchChange = (event) => {
-    this.setState({searchFeild: event.target.value})
-  }
-
-  render(){
-    const {robots,searchFeild} = this.state
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchFeild.toLowerCase())
     })
+
+    
       return !robots.length ? 
       <div className='main-bg h-screen '><h1 className='main_header'>Loading...</h1></div>:
       (<div className='main-bg'>
         <h1 className='main_header'>RoboFriends</h1>
-        <SearchBox searchFeild={this.onSearchChange} />
+        <SearchBox searchFeild={onSearchChange} />
         <Scroll>
           <ErrorBoundary><CardList robots={filteredRobots}/></ErrorBoundary>
           
         </Scroll>
         </div>)
-  }
 }
 
 export default App
